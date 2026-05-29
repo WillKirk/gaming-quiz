853236ad7b1c08295141202169969be3a8e33599
WillKirk <willkirkpatrick097@gmail.com>
2026-05-29 17:03:09 +0100

    added api call to open trivia db

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
-console.log('app.js loaded');
+const API_URL = 'https://opentdb.com/api.php?amount=10&category=15&type=multiple';
+
+let questions = [];
+let currentIndex = 0;
+let score = 0;
+
+function decodeHTML(html) {
+    const textarea = document.createElement('textarea');
+    textarea.innerHTML = html;
+    return textarea.value;
+}
+
+async function fetchQuestions() {
+    // const response = await fetch(API_URL);
+    const response = await fetch('questions.json');
+    const data = await response.json();
+
+    console.log(data);
+
+    const questions = data.map(function(question){
+        return {
+            question: decodeHTML(question.question),
+            correct_answer: decodeHTML(question.correct_answer),
+            incorrect_answers: question.incorrect_answers.map(decodeHTML),
+        };
+    });
+
+    console.log(questions);
+}
+
+fetchQuestions();
+
