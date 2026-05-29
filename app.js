f6ec80e616baa24494a119e49a0eb1f075269919
WillKirk <willkirkpatrick097@gmail.com>
2026-05-29 17:17:40 +0100

    added question and answers buttons

--- app.js
+++ app.js
@@ -10,6 +10,41 @@ function decodeHTML(html) {
     return textarea.value;
 }
 
+function shuffleAnswers(question){
+    const answers = [
+        question.correct_answer,
+        question.incorrect_answers[0],
+        question.incorrect_answers[1],
+        question.incorrect_answers[2],
+    ]
+
+    for (let i = answers.length - 1; i > 0; i--){
+        const j = Math.floor(Math.random() * (i+1));
+        const temp = answers[i];
+        answers[i] = answers[j];
+        answers[j] = temp;
+    }
+
+    return answers;
+}
+
+function renderQuestion() {
+    const question = questions[currentIndex];
+    const answers = shuffleAnswers(question);
+
+    const questionText = document.getElementById('question-text');
+    questionText.textContent = question.question;
+
+    const answersContainer = document.getElementById('answers-container');
+    answersContainer.innerHTML = '';
+
+    for (let i = 0; i < answers.length; i++) {
+        const button = document.createElement('button');
+        button.textContent = answers[i];
+        answersContainer.appendChild(button);
+    }
+}
+
 async function fetchQuestions() {
     // const response = await fetch(API_URL);
     const response = await fetch('questions.json');
@@ -17,7 +52,7 @@ async function fetchQuestions() {
 
     console.log(data);
 
-    const questions = data.map(function(question){
+    questions = data.map(function(question){
         return {
             question: decodeHTML(question.question),
             correct_answer: decodeHTML(question.correct_answer),
@@ -25,7 +60,7 @@ async function fetchQuestions() {
         };
     });
 
-    console.log(questions);
+    renderQuestion();
 }
 
 fetchQuestions();
